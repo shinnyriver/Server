0ab5658ec13c01063c05dfaa21410372b66a9c35
Changsu Park <gd12091@gmail.com>
2023-12-20 23:05:22 +0900

    FEAT : ENV, Gitignore

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s1tsj!+a%sj1y-^4zm%b_l5igl8nz9gujav4k4#zxj#9dl72ox'
+SECRET_KEY = {'SECRET_KEY'}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
